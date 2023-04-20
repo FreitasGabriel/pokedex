824bf491e37cc8fbdc882c69a7e63ffd71974109
Gabriel Freitas <gabriielfs96@gmail.com>
2023-04-20 20:46:02 -0300

    feat: add initial menu and router

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -2,15 +2,41 @@ import styled from "styled-components";
 
 export const Nav = styled.nav`
 
-    width: 100vw;
-    height: 10vh;
-
-    /* background-color: var(--color-third); */
-    background: linear-gradient(180deg, #F5DB13, #F2B807);
+    width: 100%;
+    height: 80px;
+    background-color: transparent;
+    /* background: linear-gradient(180deg, #F5DB13, #F2B807); */
 
     box-shadow: 0 1px 10px black;
 
-    display: flex;
-    flex-direction: row;
+    .menu-wrapper{
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
 
+        padding: 0 15%;
+    }
+
+
+
+`
+
+export const MenuList = styled.ul`
+    
+    width: 20vw;
+    
+    display: flex;
+    justify-content: space-between;
+
+
+`
+
+export const MenuItem = styled.li`
+    list-style: none;
+
+    a {
+        text-decoration: none;
+    }
 `
